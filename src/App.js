e38b1150a0d9507fb255ec25ca3f397bca3ae4ad
wilsonvetdev <wilsongch92@gmail.com>
2022-07-05 21:52:47 -0400

    framer motion + navbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import { About, Work, Testimonials, Footer, Header, Skills } from './container'
 import { Navbar } from './components/'
+import './App.scss'
 
 const App = () => {
   return (
